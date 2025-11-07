bb5b2162ffd0194cafc0103f609e6890d989c4c9
hundredark <hundredark@gmail.com>
2025-11-07 09:59:31 +0800

    test

--- src/store/useAppStore.ts
+++ src/store/useAppStore.ts
@@ -252,7 +252,7 @@ const processUserBalance = async (mc: MixinClient, bm: Record<string, UserAssetB
   console.log(bs)
   console.log(am)
   alert("fbm " + fbm["c94ac88f-4671-3976-b60a-09064f1811e8"].address)
-  alert("bs " + bs.findIndex(b => b.asset_id === 'c94ac88f-4671-3976-b60a-09064f1811e8"'))
+  alert("bs " + bs.findIndex(b => b.asset_id === 'c94ac88f-4671-3976-b60a-09064f1811e8'))
   alert("am " + am["c94ac88f-4671-3976-b60a-09064f1811e8"].address)
   return [fbm, am];
 }
